fe2c833454bd1975462598c772bc67f80d9fdaf7
Abdulrozzaq <amusarosaq27@gmail.com>
2026-06-03 08:06:55 +0100

    week1 day1: variable, datatype and operators

---
+++ Month-1-Javascript/week-1-Js Basics/operators.js
@@ -0,0 +1,36 @@
+//**Arimethic operators  +, -, %, *, /, **
+
+let x = 20, y  = 10
+
+console.log(x+y) //
+console.log (x-y) //
+console.log(x**2) //
+console.log(x%y)  //
+console.log(x/y)  //
+
+//**Rational/comparision operators < > >= <= =!  
+//Always rerurn Boolean value
+
+console.log (x >= y) // true
+
+console.log( x <= y) //false 
+
+console.log(x != 20);
+
+// **Ternary operators  ? 
+
+console.log(x>y ? x:y) //return x value 20
+
+console.log(x<y ? x:y) // return y value 10 
+
+
+
+//Logical oprators &&, ||, ! 
+
+let a = true
+
+let b = false 
+
+console.log(a && b) //fasle
+
+console.log(a || b) // true
---
+++ Month-1-Javascript/week-1-Js Basics/variable-and-data-type.js
@@ -0,0 +1,53 @@
+let num = 12;
+console.log(num);
+
+var x = 100;
+
+console.log(x);
+
+// *** Constant 
+
+const skin = 'black'
+console.log(skin)
+skin =  "brown" //incorrect skin value is constant 
+console.log(skin)
+
+
+
+//***Differnce between var and let
+
+console.log(y)
+
+var y = 200;
+
+console.log(y)
+
+
+console.log(z)
+let z = 170
+
+console.log(z)
+
+
+//****Datatype
+
+let race = 'white'
+console.log(typeof(race));
+
+let word = true
+console.log(typeof(word))
+
+let id = 123
+console.log(typeof(id))
+
+let GPA = 3.9
+
+console.log(typeof(GPA))
+
+let value = null 
+
+console.log(typeof(value))
+
+
+let name = undefined
+console.log(typeof(name))

3a472d2ae78c4fc60a1f00cfd0fa4ef28eadf9f7
Abdulrozzaq <amusarosaq27@gmail.com>
2026-06-17 23:05:53 +0100

    Add object exercise

---
+++ Month-1-Javascript/week-3-objects+ES6+/jsobject.js
@@ -0,0 +1,45 @@
+let person = {
+    first_name: "John",
+    last_name: "Jones",
+    age: 34,
+    weight: 50
+
+};
+
+//**Dot Notation */
+console.log(person.age);
+
+person.height = 43;
+
+console.log(person.height);
+
+
+delete person.age
+
+console.log(person.age)
+
+let employee = {
+    name: "ben",
+    job : "Engineer",
+    salary: 50000,
+    bonus : function(){
+        return((this.salary/100)*10)
+    }
+};
+
+console.log(employee.bonus());
+
+//**Bracket Notation */
+
+employee["race"] = "black"
+
+console.log(employee["race"]);
+
+//**Destructuring */
+
+let {name,job} = employee;
+
+for(x in employee){
+    console.log(x[employee]);
+}
+

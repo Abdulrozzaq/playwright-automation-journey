2ace041f90dde0c94df55a41adaec19e89b397b0
Abdulrozzaq <amusarosaq27@gmail.com>
2026-06-22 22:00:21 +0100

    update js modules exercise

--- Month-1-Javascript/week-3-objects+ES6+/jsmodules.js
+++ Month-1-Javascript/week-3-objects+ES6+/jsmodules.js
@@ -3,6 +3,16 @@ export function add(a, b) {
   return a + b;
 }
 
+export const name = "Adam";
+export const age = 89;
+
+//**Export together */
+
+const age = 90;
+const name = "Adams"
+
+export {age, name}
+
 
 // Import an "add" function
 import { multiply} from './function.js';

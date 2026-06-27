a51bf0c76a8b76013a1c6e042763570c7cb3d225
Abdulrozzaq <amusarosaq27@gmail.com>
2026-06-27 23:55:09 +0100

    Add nested object exercise

---
+++ Month-1-Javascript/week-3-objects+ES6+/nestedobject.js
@@ -0,0 +1,12 @@
+const person = { 
+    fullName : "John jone",
+    isFighter : true,
+    skills:  ["grappling", "striking", "wrestling"],
+    address : {
+        country : "us",
+        state : "florida",
+        street : "ufc 123 close"
+    }
+}
+
+console.log(person.address.country);

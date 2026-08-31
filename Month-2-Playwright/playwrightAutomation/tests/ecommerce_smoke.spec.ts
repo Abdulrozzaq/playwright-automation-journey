5a84c8884a84c42bbb3b897df2abaa42db3a5055
Abdulrozzaq <amusarosaq27@gmail.com>
2026-08-31 15:51:09 +0100

    complete practice project

--- Month-2-Playwright/playwrightAutomation/tests/ecommerce_smoke.spec.ts
+++ Month-2-Playwright/playwrightAutomation/tests/ecommerce_smoke.spec.ts
@@ -3,14 +3,15 @@ import {test, expect} from "@playwright/test"
 
 test("verify webste", async ({page}) => {
 
+    //Navigate and Register 
+
     const email = 'testbud' + Date.now() + '@qa.com'
 
-    await page.goto("https://tricentis.com"); 
+    await page.goto("https://demowebshop.tricentis.com");
 
+    await  page.getByRole('link', {name: "Register", exact :true,}).click();
 
-    const link = page.getByRole('link', {name: "Register", exact :true,}).click();
-
-    await page.getByLabel("Gender").check();
+    await page.getByLabel("Male",{exact:true}).check();
 
     await  page.getByLabel("First name").fill("Testing");
 
@@ -18,15 +19,56 @@ test("verify webste", async ({page}) => {
 
     await page.getByLabel("Email").fill(email);
 
-    await page.getByLabel("Password").fill("Testme@123!");
+    await page.locator("label[for='Password']").fill("Testeme@123!");
+
+    await page.getByLabel("Confirm password").fill("Testme@123!");
 
     await page.getByRole("button",{name : "Register", exact :  true,}).click();
 
 
-    
-
-    
-
 });
 
+
+test("'verify search", async ({page}) => {
+
+    const productName  = "Computing and Internet";
+
+     await page.goto("https://demowebshop.tricentis.com");
+
+
+    const searchinput = page.locator("#small-searchterms")
+
+    await searchinput.fill(productName);
+
+    await page.getByRole("button",{name:"Search"}).click();
+
+
+    const complink = page.getByRole("link", {name: productName, exact:true,}); 
+
+    await expect(complink).toBeVisible();
+
+    await complink.click();
+
+    // Adding item to cart 
+
+    const booknum = page.locator(".qty-input");
+
+
+    await booknum.fill('3');
+
+    await page.getByRole("button", {name : "Add to cart"}).first().click();
+
+
+    //verify cart page
+
+    await page.getByRole("link", {name: "Shopping cart", exact : true,}).click();
+    await expect(page).toHaveURL(/\/cart/);
+
+    const catitem = page.locator(".cart-item-row", {hasText : productName}); 
+
+    await expect(catitem).toBeVisible();
+
+
+
+});
 

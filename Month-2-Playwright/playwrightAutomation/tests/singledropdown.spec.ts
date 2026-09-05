f51381af8e9a99c9528a288df457c913003863b0
Abdulrozzaq <amusarosaq27@gmail.com>
2026-09-06 00:00:17 +0100

    update dropdown lesson

--- Month-2-Playwright/playwrightAutomation/tests/singledropdown.spec.ts
+++ Month-2-Playwright/playwrightAutomation/tests/singledropdown.spec.ts
@@ -3,5 +3,8 @@ import {test,expect} from '@playwright/test'
 test('single dropdown', async ({page}) => {
 
     await page.locator("#country").selectOption('india');
+        await page.locator("#country").selectOption('india');
+    await page.locator("#country").selectOption('india');
+
 
 });
